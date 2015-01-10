497ba17b52f963ecee7ef8795ade74ed94cc2eb4
Zack Evans <zackalta15@gmail.com>
2015-01-10 00:11:10 -0500

    cleanup

--- Chapter 6/Exercise P6.13/src/drunk/GridComponents.java
+++ Chapter 6/Exercise P6.13/src/drunk/GridComponents.java
@@ -21,19 +21,19 @@ public class GridComponents extends JComponent
 	public static void drawGrid(Graphics g)
 	{
 		Graphics2D g2 = (Graphics2D) g;
-        int lane = 20;
-        int rowCount = 20;
-        int columnCount = 20;
-		
+	        int lane = 20;
+	        int rowCount = 20;
+	        int columnCount = 20;
+			
 		for (int row = 0; row < rowCount; row++)
 		{
 			g2.drawLine(0, row*lane, rowCount*lane, row*lane);
 		}
-			
-        for (int col = 0; col < columnCount; col++)
-        {
-        	g2.drawLine(col*lane,0,col*lane,columnCount*lane);
-        }	
+				
+	        for (int col = 0; col < columnCount; col++)
+	        {
+	        	g2.drawLine(col*lane,0,col*lane,columnCount*lane);
+	        }	
 	}
 	
 	public void drawDot (Graphics g)
@@ -47,10 +47,19 @@ public class GridComponents extends JComponent
 		{
 			Random rn = new Random();
 			int movenum = rn.nextInt(4) + 1;
-			if (movenum == 1){xcord = xcord+20;}
-			if (movenum == 2){ycord = ycord +20;}
-			if (movenum == 3){xcord = xcord -20;}
-			if (movenum == 4){ycord = ycord -20;}
+			if (movenum == 1){
+				xcord = xcord+20;
+			}
+			if (movenum == 2){
+				ycord = ycord +20;
+			}
+			if (movenum == 3){
+				xcord = xcord -20;
+			}
+			if (movenum == 4){
+				ycord = ycord -20;
+			}
+			
 			circle.y = ycord;
 			circle.x = xcord;
 			g2.fill(circle);
